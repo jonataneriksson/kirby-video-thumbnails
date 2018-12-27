67b2c54a76d68de69f7c458f02578f3fc70d3ebd
Jonatan Eriksson <joantan.eriksson@gmail.com>
2018-12-27 16:19:05 -0500

    Fixes from duotone.fi

--- lib/component.php
+++ lib/component.php
@@ -107,11 +107,9 @@ class VideoThumb extends Thumb {
       $ffprobeinfo = json_decode(shell_exec($probe), true);
 
       //Sort object
-      $metadata = [];
-      $metadata['width'] = $ffprobeinfo['streams'][0]['width'];
-      $metadata['height'] = $ffprobeinfo['streams'][0]['height'];
-      $metadata['duration'] = $ffprobeinfo['format']['duration'];
-      $this->metadata = $metadata;
+      $this->metadata['width'] = $ffprobeinfo['streams'][0]['width'];
+      $this->metadata['height'] = $ffprobeinfo['streams'][0]['height'];
+      $this->metadata['duration'] = $ffprobeinfo['format']['duration'];
       return $this->metadata;
     } else {
       return $this->metadata;
